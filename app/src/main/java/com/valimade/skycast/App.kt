2e6434ed4c5da24b0c42385a8a0aac60724a359d
Danil Valihmedov <valiahmedov2013@mail.ru>
2025-07-23 22:09:22 +0300

    app:generateDebugPlacesDBInterface FAILED

--- app/src/main/java/com/valimade/skycast/App.kt
+++ app/src/main/java/com/valimade/skycast/App.kt
@@ -1,6 +1,7 @@
 package com.valimade.skycast
 
 import android.app.Application
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import com.valimade.skycast.geocoding.di.geocodingModule
 import com.valimade.skycast.location.di.locationModule
 import com.valimade.skycast.permission.di.permissionModule
@@ -11,6 +12,10 @@ import org.koin.core.context.startKoin
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
+
+//        val driver = AndroidSqliteDriver(PlacesDB.Schema, context, "places.db")
+//        val database = PlacesDB(driver)
+
         startKoin {
             androidContext(this@App)
             modules(
